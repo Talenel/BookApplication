7d9bd56ec614d65bb0680f9e0141a62fe0c7bf69
Justin Gross <jmbg144@gmail.com>
2017-06-05 13:53:54 -0400

    enhancing book class

--- src/Book.java
+++ src/Book.java
@@ -11,6 +11,8 @@ public class Book {
 	private int stock;
 	
 	
+	
+	
 	public Book(String aTitle, String anAuthor, String aDescription) {
 		// TODO Auto-generated constructor stub
 		title= aTitle;
@@ -19,12 +21,67 @@ public class Book {
 		
 	}
 	
+	
+	
 	public Book(String aTitle, String anAuthor, String aDescription, double aPrice) {
 		title= aTitle;
 		author=anAuthor;
 		description=aDescription;
 		price=aPrice;
 			
+	}
+	public Book(String sku)
+	{
+		if(sku.equals("Java1001"))
+		{
+			title="Head First Java";
+			author="Kathy Sierra and Bert Bates";
+			description="Easy to read Java workbook";
+			price=47.50;
+			
+		}
+		else if(sku.equals("Java1002"))
+		{
+			title="Thinking in Java";
+			author="Bruce Eckel";
+			description="Details about Java under the hood";
+			price=20.00;
+		}
+		else if(sku.equals("Orcl1003"))
+		{
+			title="OCP:Oracle Certified Professional Java SE";
+			author="Jeanne Boyarsky";
+			description="Everything you need to know in one place";
+			price=45.00;
+		}
+		else if(sku.equals("Python1004"))
+		{
+			title="Automate the Boring Stuff with Python";
+			author="Al Sweigart";
+			description="Fun with Python"; 
+			price=10.50;
+		}
+		else if(sku.equals("Zombie1005"))
+		{
+			title="The Maker's Guide to the Zombie Apocalypse";
+			author="Simon Monk";
+			description="Defend Your Base with Simple Circuits, Arduino, and Raspberry Pi";
+			price=16.50;
+		}
+		else if(sku.equals("Rasp1006"))
+		{
+			title="Raspberry Pi Projects for the Evil Genius";
+			author="Donald Norris";
+			description="A dozen fiendishly fun projects for the Raspberry Pi";
+			price=14.75;
+		}
+		else
+		{
+			title="Not found";
+			author="Unknown";
+			description="Not a valid sku";
+			price=0;
+		}
 	}
 	
 	public Book(String aTitle, String anAuthor, String aDescription, double aPrice, boolean anIsInStock) {
@@ -123,9 +180,9 @@ public class Book {
 	}
 	
 	
-	public void getDisplayText()
+	public String getDisplayText()
 	{
-		System.out.printf("Title: %s\nAuthor: %s\nDescription: %s\n", title, author, description);
+		return "Title: "+title+"%s\nAuthor: "+author+"\nDescription: "+description+"\n";
 	}
 }
 
--- src/BookApp.java
+++ src/BookApp.java
@@ -5,7 +5,7 @@ public class BookApp {
 		// TODO Auto-generated method stub
 		
 		Book lotr= new Book("Lord of the Rings", "J.R.R. Tolkien","The Fantastical Epic story of a journey save the world", 10.55);
-		lotr.getDisplayText();
+		System.out.println(lotr.getDisplayText());
 		
 		
 		System.out.println(lotr.getPricing(5));
